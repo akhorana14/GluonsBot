34ead0e749544f0c9861494109ef53daa0fd7a18
akhorana14 <31749972+akhorana14@users.noreply.github.com>
2019-10-01 14:52:47 -0500

    added !help and !ping, error trapped

--- src/main/java/events/HelloEvent.java
+++ src/main/java/events/HelloEvent.java
@@ -1,28 +1,42 @@
 package events;
 
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.utils.MiscUtil;
+
+import java.util.Date;
 
 public class HelloEvent extends ListenerAdapter {
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent event)
     {
         String message = event.getMessage().getContentRaw();
+
+
+
+
         if(message.charAt(0) == '!')
         {
-            if(message.equals("!help"))
+            if(message.equalsIgnoreCase("!help"))
             {
-                event.getChannel().sendMessage("```**Hello, I'm GluonsBot!**\n" +
+                event.getChannel().sendMessage("**Hello, I'm GluonsBot!**\n" +
                         "Below you can see all the commands I know.\n" +
                         "Have a nice day!\n\n" +
                         "" +
-                        "!help - Displays help command\n" +
-                        "hsajsakdaslhjsk```").queue();
+                        "`!help` - Displays help command\n" +
+                        "`!ping` - Displays current latency\n").queue();
             }
-            if(message.equals("!ping"))
+            else if(message.equalsIgnoreCase("!ping"))
             {
-                event.getChannel().sendMessage("Pong! " /*+ onEvent(GenericEvent).getChannel().getTimeCreated() - event.getChannel().getTimeCreated()*/).queue();
+                event.getChannel().sendMessage("Ping!")
+                int m = event.getChannel().getLastMessage().getTimeCreated().getNano();
+                event.getChannel().sendMessage("Pong! "+m+"ms");
+            }
+            else
+            {
+                event.getChannel().sendMessage("Invalid Command! See !help for details.").queue();
             }
         }
 
